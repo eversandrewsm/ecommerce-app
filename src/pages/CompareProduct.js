9f755b1933038349ffc46b369950d91497ecd7a8
eversandrews@gmail.com <eversandrews@gmail.com>
2024-04-08 03:06:36 +0200

    08/04/2024-3

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import Color from "./Color";
 
 const CompareProduct = () => {
   return (
@@ -24,7 +25,7 @@ const CompareProduct = () => {
                   <h5 className="title">
                     Honor T1 7.0 1 GB RAM 8 GB ROM 7 INCH With Wi-Fi +3 G Tablet
                   </h5>
-                  <h6 className="price mb-3">R1000.00</h6>
+                  <h6 className="price mb-3 mt-3">R1000.00</h6>
                   <div>
                     <div className="product-detail">
                       <h5>Brand:</h5>
@@ -44,7 +45,56 @@ const CompareProduct = () => {
                     </div>
                     <div className="product-detail">
                       <h5>Color:</h5>
+                      <Color />
+                    </div>
+                    <div className="product-detail">
+                      <h5>Size:</h5>
+                      <div className="d-flex gap-10">
+                        <p>S</p>
+                        <p>M</p>
+                        <p>L</p>
+                        <p>XL</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="compare-product-card position-relative">
+                <img
+                  src="images/cross.svg"
+                  alt="cross"
+                  className="position-absolute cross img-fluid"
+                />
+                <div className="product-card-image">
+                  <img src="images/watch.jpg" alt="watch" />
+                </div>
+                <div className="compare-product-details">
+                  <h5 className="title">
+                    Honor T1 7.0 1 GB RAM 8 GB ROM 7 INCH With Wi-Fi +3 G Tablet
+                  </h5>
+                  <h6 className="price mb-3 mt-3">R1000.00</h6>
+                  <div>
+                    <div className="product-detail">
+                      <h5>Brand:</h5>
                       <p>Havels</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Type:</h5>
+                      <p>Wearables</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>SKU:</h5>
+                      <p>85588888888</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Availability:</h5>
+                      <p>In Stock</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Color:</h5>
+                      <Color />
                     </div>
                     <div className="product-detail">
                       <h5>Size:</h5>
